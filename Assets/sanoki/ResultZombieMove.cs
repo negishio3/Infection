b69e97df281482b478e81cac5e22dc886145f344
sanoki <nkc.hirokazu.sanoki@gmail.com>
2018-05-29 13:38:04 +0900

    リザルト画面作成中

---
+++ Assets/sanoki/ResultZombieMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultZombieMove : MonoBehaviour {
+    BoxCollider col;
+    Animator ani;//animator
+    Vector3 pos;//座標
+    Vector3 qua;//回転
+    float moveSpeed = 10.0f;//落ちる速度
+    bool moveflg = true;//動くフラグ
+    private void Start()
+    {
+        col = GetComponent<BoxCollider>();
+        ani = GetComponent<Animator>();//Animatorコンポーネントを取得
+        pos = transform.position;//初期位置を代入
+        qua.y = Random.value*360;
+        transform.Rotate(qua);
+    }
+
+    void Update()
+    {
+        if (moveflg)
+        {
+            pos.y -= moveSpeed * Time.deltaTime;//Y座標の値をひたすらマイナス
+            transform.position = pos;//オブジェクトの座標に反映
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "grownd")
+        {
+            moveflg = false;
+            pos.y = transform.position.y+col.size.y / 2;
+            transform.position = pos;
+            ani.SetTrigger("landing");//地面に触れたら落下モーションを再生
+        }
+    }
+
+}
--- Assets/sanoki/ZombieInstant.cs
+++ Assets/sanoki/ZombieInstant.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ZombieInstant : MonoBehaviour {
-    public Vector3 instantPos;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public GameObject[] instantPos;
+    public GameObject zombiePre;
+
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instans();
+        }
 	}
+    void Instans()
+    {
+        for (int i = 0; i < instantPos.Length; i++) {
+            
+            Instantiate(zombiePre, instantPos[i].transform.position, Quaternion.identity);
+        }
+    }
     //public IEnumerator ScoreCount()
     //{
 
