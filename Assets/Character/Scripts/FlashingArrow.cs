bc4c21c3da4619dbc2a0d1b014b2a73e7ce1d0de
takagi <nkc.ryousuke.takagi@gmail.com>
2018-06-06 14:21:35 +0900

    その矢印は点滅します

---
+++ Assets/Character/Scripts/FlashingArrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FlashingArrow : MonoBehaviour {
+
+    private float alpha=0;
+    private float minus = 0.3f;
+    private bool flg=true;
+    private MeshRenderer meshrender;
+    void Start () {
+        meshrender = GetComponent<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (alpha < 0) { minus *= -1; }
+        else if(alpha>1){ minus *= -1; }
+        alpha += Time.deltaTime * minus;
+        Debug.Log(alpha);
+        meshrender.material.color = new Color(1, 1, 1, alpha);
+    }
+}
