0edf55fd7d7684a4623597e2d34ed6ff89b702ba
mase <nkc.yuya.mase.2016@gmail.com>
2018-06-15 11:14:57 +0900

    げろ追加中

---
+++ Assets/mase/Scripts/gero.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gero : MonoBehaviour
+{
+
+    //public GameObject[] Gero;
+    public GameObject Gero;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Instantiate(Gero, this.transform.position, Quaternion.identity);
+            Debug.Log("気持ち悪い");
+
+        }
+
+    }
+}
